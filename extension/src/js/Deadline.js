6b9f831ffd34f1b1083f68d06bba9d32ea487b6e
mdc <hoangmanh.16072001@gmail.com>
2024-03-04 11:08:05 +0700

    update

--- extension/src/js/Deadline.js
+++ extension/src/js/Deadline.js
@@ -8,11 +8,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const deadlineValue = deadlineInput.value;
 
     if (deadlineValue) {
+      const deadlineDate = new Date(deadlineValue);
+
+      if (isNaN(deadlineDate.getTime())) {
+        // Hiển thị thông báo nếu định dạng thời gian không hợp lệ
+        Swal.fire({
+          icon: "error",
+          title: "Invalid date format",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+        return;
+      }
+
       // Thêm task vào danh sách lịch sử
       const taskItem = document.createElement("li");
-      const taskText = `Task deadline: ${deadlineValue}`;
+      const taskText = `Task deadline: ${deadlineDate.toLocaleString()}`;
       taskItem.textContent = taskText;
       taskHistoryList.appendChild(taskItem);
+      alarmSound.play();
 
       // Thông báo và chơi âm thanh khi commit task thành công
       Swal.fire({
@@ -21,8 +35,6 @@ document.addEventListener("DOMContentLoaded", function () {
         showConfirmButton: false,
         timer: 2000,
       });
-
-      alarmSound.play();
 
       // Lấy văn bản công việc và thời gian
       const taskWithTime = `${taskText} - ${new Date().toLocaleString()}`;
